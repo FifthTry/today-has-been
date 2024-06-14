a491fb21a5c19772ddd632aea0a8b3eeab7d22bf
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-06-14 10:41:36 +0530

    Created login page

---
+++ backend/src/login.rs
@@ -0,0 +1,19 @@
+#[ft_sdk::processor]
+fn login(
+    ft_sdk::Query(access_token): ft_sdk::Query<"access-token">,
+    ft_sdk::Query(order): ft_sdk::Query<"order", Option<String>>,
+    ft_sdk::Query(next): ft_sdk::Query<"next", Option<String>>,
+    host: ft_sdk::Host,
+) -> ft_sdk::processor::Result {
+    let next = next.unwrap_or_else(|| {
+        let order_query = order
+            .filter(|order| !order.is_empty())
+            .map(|order| format!("?order={}", order))
+            .unwrap_or_default();
+
+        format!("/{}", order_query)
+    });
+
+    Ok(ft_sdk::processor::temporary_redirect(next)?
+        .with_cookie(todayhasbeen::session_cookie(access_token.as_str(), host)?))
+}
---
+++ backend/src/user.rs
@@ -0,0 +1,128 @@
+#[ft_sdk::data]
+fn user(
+    ft_sdk::Query(order): ft_sdk::Query<"order", Option<String>>,
+    cookie: ft_sdk::Cookie<{ ft_sdk::auth::SESSION_KEY }>,
+    host: ft_sdk::Host,
+) -> ft_sdk::data::Result {
+    let access_token = cookie.0;
+
+    let order = order.unwrap_or("new".to_string());
+
+    match access_token {
+        Some(access_token) => {
+            let posts = get_posts_by_order(access_token.as_str(), order.as_str());
+            Ok(ft_sdk::data::json(UserData {
+                is_logged_in: true,
+                posts,
+            })?
+            .with_cookie(todayhasbeen::session_cookie(access_token.as_str(), host)?))
+        }
+        None => ft_sdk::data::json(UserData {
+            is_logged_in: false,
+            posts: vec![],
+        }),
+    }
+}
+
+fn get_posts_by_order(access_token: &str, _order: &str) -> Vec<PostData> {
+    let get_posts = call_get_posts_api(access_token);
+    let mut post_data_hash: std::collections::HashMap<String, Vec<PostDataByDate>> =
+        std::collections::HashMap::new();
+    for post in get_posts.data {
+        let naive_date_time = string_to_naive_date_time(post.createdon.as_str());
+        let date = naive_date_time.date().to_string();
+        let post_by_date = PostDataByDate {
+            time: naive_date_time.time().to_string(),
+            post: post.postcontent,
+            media_url: post.mediaurl,
+        };
+        match post_data_hash.get_mut(&date) {
+            Some(posts) => posts.push(post_by_date),
+            None => {
+                post_data_hash.insert(date, vec![post_by_date]);
+            }
+        }
+    }
+    post_data_hash
+        .into_iter()
+        .map(|(date, post_data_by_date)| PostData {
+            date,
+            data: post_data_by_date,
+        })
+        .collect()
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct ApiResponse {
+    status: bool,
+    data: Vec<ApiPost>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct ApiPost {
+    postid: i32,
+    userid: i32,
+    postcontent: Option<String>,
+    mediaurl: Option<String>,
+    createdon: String,
+}
+
+fn call_get_posts_api(token: &str) -> ApiResponse {
+    let authorization_header = format!("Bearer {}", token);
+
+    let client = http::Request::builder();
+    let request = client
+        .method("GET")
+        .uri("http://ec2-184-72-72-45.compute-1.amazonaws.com/api/v0.1/get/posts")
+        .header("Authorization", authorization_header)
+        .header("Accept", "application/json; api-version=2.0")
+        .header("Content-Type", "application/json")
+        .header("User-Agent", "FifthTry")
+        .body(bytes::Bytes::new())
+        .unwrap();
+
+    let response = ft_sdk::http::send(request).unwrap(); //todo: remove unwrap()
+
+    if response.status().is_success() {
+        let api_response: ApiResponse = serde_json::from_str(
+            String::from_utf8_lossy(response.body())
+                .to_string()
+                .as_str(),
+        )
+        .unwrap();
+        // Extract the 'value' field from the JSON response
+        ft_sdk::println!("Response: {:?}", api_response);
+
+        api_response
+    } else {
+        ft_sdk::println!("Request failed with status: {}", response.status());
+        todo!()
+    }
+}
+
+#[derive(serde::Serialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct UserData {
+    is_logged_in: bool,
+    posts: Vec<PostData>,
+}
+
+#[derive(serde::Serialize, Debug)]
+struct PostData {
+    date: String,
+    data: Vec<PostDataByDate>,
+}
+
+#[derive(serde::Serialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+struct PostDataByDate {
+    time: String,
+    post: Option<String>,
+    media_url: Option<String>,
+}
+
+fn string_to_naive_date_time(date_time_str: &str) -> chrono::NaiveDateTime {
+    let format = "%Y-%m-%d %H:%M:%S";
+    chrono::NaiveDateTime::parse_from_str(date_time_str, format)
+        .expect("Failed to parse date and time")
+}
