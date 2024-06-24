58ba12839093c7fd2d55576065efbb5baf6a480a
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-06-24 20:53:38 +0530

    Added charge subscription

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -12,10 +12,13 @@ mod register;
 mod schema;
 mod user;
 mod payment_link;
+mod charge_subscription;
 
 const SECRET_KEY: &str = "SECRET_KEY";
 const STRIPE_SECRET_KEY: &str = "STRIPE_SECRET_KEY";
 const DURATION_TO_EXPIRE_ACCESS_TOKEN_IN_DAYS: i64 = 60;
+const GUPSHUP_CALLBACK_SERVICE_URL: &str = "https://notifications.gupshup.io/notifications/callback/service/ipass/project/730/integration/19770066040f26502c05494f2";
+const GUPSHUP_AUTHORIZATION: &str = "1fe499df-e424-46a0-af05-802d97353d42";
 
 pub(crate) fn set_session_cookie(
     sid: &str,
@@ -86,6 +89,30 @@ pub(crate) fn get_user_from_access_token(
     Ok(user)
 }
 
+
+pub(crate) fn get_user_from_customer_id(
+    conn: &mut ft_sdk::Connection,
+    customer_id: &str,
+) -> Result<todayhasbeen::UserData, ft_sdk::Error> {
+    use diesel::prelude::*;
+    use todayhasbeen::schema::users;
+
+    // Query user based on access_token
+    let user = users::table
+        .filter(users::customer_id.eq(customer_id))
+        .select(todayhasbeen::UserData::as_select())
+        .first(conn)?;
+
+    // Check if token has expired
+    if user.is_access_token_expired() {
+        return Err(
+            ft_sdk::SpecialError::Unauthorised("Access token has expired!".to_string()).into(),
+        );
+    }
+
+    Ok(user)
+}
+
 #[derive(Debug, serde::Serialize, diesel::Selectable, diesel::Queryable)]
 #[diesel(table_name = todayhasbeen::schema::users)]
 struct UserData {
@@ -95,7 +122,7 @@ struct UserData {
     time_zone: Option<String>,
     language: Option<String>,
     subscription_type: Option<String>,
-    subscription_end_time: Option<chrono::DateTime<chrono::Utc>>,
+    subscription_end_time: Option<String>,
     customer_id: Option<String>,
     access_token: String,
     created_on: chrono::DateTime<chrono::Utc>,
@@ -127,3 +154,32 @@ fn get_access_token(headers: &http::HeaderMap) -> Result<String, ft_sdk::Error> 
         ft_sdk::SpecialError::Unauthorised("No Authorization header found.".to_string()).into()
     })
 }
+
+
+#[derive(Debug, serde::Serialize, diesel::Selectable, diesel::Queryable)]
+#[diesel(treat_none_as_default_value = false)]
+#[diesel(table_name = todayhasbeen::schema::subscription_plans)]
+pub struct SubscriptionPlan {
+    pub id: i64,
+    pub plan: String,
+    pub price_id: String,
+    pub amount: f64,
+    pub created_on: chrono::DateTime<chrono::Utc>,
+}
+
+
+#[derive(Debug, serde::Serialize, diesel::Selectable, diesel::Queryable, diesel::Insertable)]
+#[diesel(treat_none_as_default_value = false)]
+#[diesel(table_name = todayhasbeen::schema::subscriptions)]
+pub struct Subscription {
+    pub user_id: i64,
+    pub subscription_id: String,
+    pub start_date: String,
+    pub end_date: String,
+    pub status: Option<String>,
+    pub is_active: Option<String>,
+    pub plan_type: Option<String>,
+    pub created_on: chrono::DateTime<chrono::Utc>,
+    pub updated_on: chrono::DateTime<chrono::Utc>,
+}
+
--- backend/src/payment_link.rs
+++ backend/src/payment_link.rs
@@ -15,7 +15,7 @@ fn payment_link(
 
 
     let plans = get_subscription_plans(&mut conn)?;
-    let user_data = get_user_from_customer_id(&mut conn, customer_id.as_str())?;
+    let user_data = todayhasbeen::get_user_from_customer_id(&mut conn, customer_id.as_str())?;
 
     ft_sdk::processor::json(Output {
         return_url: format!("{}/api/charge/subscription/?customer_id={customer_id}", host.without_port()),
@@ -33,52 +33,17 @@ struct Output {
     customer_id: String,
     client_secret: Option<String>,
     stripe_key: String,
-    plans: Vec<SubscriptionPlan>,
+    plans: Vec<todayhasbeen::SubscriptionPlan>,
     return_url: String,
     user_data: todayhasbeen::UserData
 }
 
-
-#[derive(Debug, serde::Serialize, diesel::Selectable, diesel::Queryable)]
-#[diesel(treat_none_as_default_value = false)]
-#[diesel(table_name = todayhasbeen::schema::subscription_plans)]
-pub struct SubscriptionPlan {
-    pub id: i64,
-    pub plan: String,
-    pub price_id: String,
-    pub amount: f64,
-    pub createdon: chrono::DateTime<chrono::Utc>,
-}
-
-fn get_subscription_plans(conn: &mut ft_sdk::Connection) -> Result<Vec<SubscriptionPlan>, ft_sdk::Error> {
+fn get_subscription_plans(conn: &mut ft_sdk::Connection) -> Result<Vec<todayhasbeen::SubscriptionPlan>, ft_sdk::Error> {
     use diesel::prelude::*;
     use todayhasbeen::schema::subscription_plans;
 
     let subscription_plans = subscription_plans::table
-        .select(SubscriptionPlan::as_select())
+        .select(todayhasbeen::SubscriptionPlan::as_select())
         .load(conn)?;
     Ok(subscription_plans)
-}
-
-pub(crate) fn get_user_from_customer_id(
-    conn: &mut ft_sdk::Connection,
-    customer_id: &str,
-) -> Result<todayhasbeen::UserData, ft_sdk::Error> {
-    use diesel::prelude::*;
-    use todayhasbeen::schema::users;
-
-    // Query user based on access_token
-    let user = users::table
-        .filter(users::customer_id.eq(customer_id))
-        .select(todayhasbeen::UserData::as_select())
-        .first(conn)?;
-
-    // Check if token has expired
-    if user.is_access_token_expired() {
-        return Err(
-            ft_sdk::SpecialError::Unauthorised("Access token has expired!".to_string()).into(),
-        );
-    }
-
-    Ok(user)
 }
