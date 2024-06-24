404fd5d03e3f173900050a3d75adeda280233e10
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-06-25 02:23:59 +0530

    Payment webcomponent working

--- frontend/web-components/payment.js
+++ frontend/web-components/payment.js
@@ -5,14 +5,14 @@ class PaymentForm extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = `
-            <style>
-                /* Add your styles here */
-            </style>
-            <form id="payment-form">
-                <div id="payment-element"></div>
-                <button type="submit" id="submit">Submit</button>
-                <div id="error-message"></div>
-            </form>
+<!--            <style>-->
+<!--                /* Add your styles here */-->
+<!--            </style>-->
+<!--            <form id="payment-form">-->
+<!--                <div id="payment-element"></div>-->
+<!--                <button type="submit" id="submit" style="background-color: #00AEC4; color: white;">Subscribe</button>-->
+<!--                <div id="error-message"></div>-->
+<!--            </form>-->
         `;
     }
 
@@ -33,9 +33,10 @@ class PaymentForm extends HTMLElement {
         };
         const elements = stripe.elements(options);
         const paymentElement = elements.create('payment');
-        paymentElement.mount(this.shadowRoot.querySelector('#payment-element'));
-
-        const form = this.shadowRoot.querySelector('#payment-form');
+        // paymentElement.mount(this.shadowRoot.querySelector('#payment-element'));
+        // const form = this.shadowRoot.querySelector('#payment-form');
+        paymentElement.mount('#payment-element');
+        const form = document.getElementById('payment-form');
         form.addEventListener('submit', async (event) => {
             event.preventDefault();
             this.shadowRoot.querySelector("#submit").style.display = 'none';
@@ -62,7 +63,32 @@ class PaymentForm extends HTMLElement {
             script.onload = resolve;
             script.onerror = reject;
             document.head.appendChild(script);
+
+            document.getElementById('payment-form')
+                .replaceWith(this.createForm());
         });
+    }
+
+    createForm() {
+        const form = document.createElement('form');
+        form.id = 'payment-form';
+
+        const paymentElementDiv = document.createElement('div');
+        paymentElementDiv.id = 'payment-element';
+
+        const submitButton = document.createElement('button');
+        submitButton.type = 'submit';
+        submitButton.id = 'submit';
+        submitButton.textContent = 'Submit';
+
+        const errorMessageDiv = document.createElement('div');
+        errorMessageDiv.id = 'error-message';
+
+        form.appendChild(paymentElementDiv);
+        form.appendChild(submitButton);
+        form.appendChild(errorMessageDiv);
+
+        return form;
     }
 
 }
