9ace86c8a8009717a2afe8c04810c189b8fd7ad3
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-07-02 10:14:34 +0530

    login via wasm

--- backend/src/login.rs
+++ backend/src/login.rs
@@ -1,10 +1,10 @@
-#[ft_sdk::processor]
+#[ft_sdk::data]
 fn login(
     ft_sdk::Query(access_token): ft_sdk::Query<"access-token">,
     ft_sdk::Query(order): ft_sdk::Query<"order", Option<String>>,
     ft_sdk::Query(next): ft_sdk::Query<"next", Option<String>>,
     host: ft_sdk::Host,
-) -> ft_sdk::processor::Result {
+) -> ft_sdk::data::Result {
     let next = next.unwrap_or_else(|| {
         let order_query = order
             .filter(|order| !order.is_empty())
@@ -14,10 +14,8 @@ fn login(
         format!("/{}", order_query)
     });
 
-    Ok(
-        ft_sdk::processor::browser_redirect(next)?.with_cookie(todayhasbeen::set_session_cookie(
-            access_token.as_str(),
-            &host,
-        )?),
-    )
+    ft_sdk::data::browser_redirect_with_cookie(next, todayhasbeen::set_session_cookie(
+        access_token.as_str(),
+        &host,
+    )?)
 }
