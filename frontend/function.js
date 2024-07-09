e34bcd3508155898750b45b77683ba475f24efa0
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-07-09 10:22:43 +0530

    Added code credit

--- frontend/function.js
+++ frontend/function.js
@@ -2,7 +2,7 @@ function first_plan_price(plans){
     return plans.get().get(0).item.get("price_id").get();
 }
 
-
+// Not calling this function in the frontend.
 function setTimezone() {
     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     ftd.http("/api/v0.1/user/timezone/", "POST", null, { timezone: timeZone });
@@ -11,7 +11,6 @@ function setTimezone() {
 
 
 // Code taken from https://stackoverflow.com/questions/1091372/getting-the-clients-time-zone-and-offset-in-javascript
-
 function setTimezoneOffset() {
     var offset = new Date().getTimezoneOffset(), o = Math.abs(offset);
     const timeZone = (offset < 0 ? "+" : "-") + ("00" + Math.floor(o / 60)).slice(-2) + ":" + ("00" + (o % 60)).slice(-2);
